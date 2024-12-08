a289ba4bbde8ec2e28784c8db8e477c288909785
buitin1104 <buitin1104@gmail.com>
2024-12-08 16:30:02 +0700

    feat: create bus trip api

--- backend/models/busTrip.schema.js
+++ backend/models/busTrip.schema.js
@@ -2,29 +2,24 @@ import mongoose from "mongoose";
 import { BusTripStatusConstant } from "../constants/busTripStatus.constant.js";
 
 const BusTripSchema = new mongoose.Schema({
-  // TODO: Change String type to ObjectId, ref to user later
-  assignedToUser: { type: String },
-  bus: { type: mongoose.Schema.Types.ObjectId, ref: "Bus", required: true },
-  origin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Location",
-    required: true,
-  },
-  destination: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Location",
-    required: true,
-  },
-  departureTime: { type: Date, required: true },
-  arrivalTime: { type: Date, required: true },
-  price: { type: Number, required: true },
-  status: {
-    type: String,
-    required: true,
-    enum: Object.values(BusTripStatusConstant),
-  },
-  // TODO: Change String type to ObjectId, ref to ticket later
-  bookedTickets: { type: [String], default: [] },
+    // TODO: Change String type to ObjectId, ref to user later
+    bus: { type: mongoose.Schema.Types.ObjectId, ref: "Bus", required: true },
+    assignedToUser: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(BusTripStatusConstant),
+    },
+    origin: [{ type: String, required: true }],
+    destination: [{ type: String, required: true }],
+    departureTime: { type: Date, required: true },
+    arrivalTime: { type: Date, required: true },
+    price: { type: Number, required: true },
+    amenity: [{ type: String, required: false }],
+    policy: { type: String, required: false },
+    ticket_booked: [{ type: Number, required: false }],
+    payment_methods: [{ type: Number, required: false }],
+    // bookedTickets: { type: [String], default: [] },
 });
 
 export default mongoose.model("BusTrip", BusTripSchema);
--- backend/routes/busTrip.route.js
+++ backend/routes/busTrip.route.js
@@ -1,214 +1,221 @@
 import express from "express";
-import BusTrip from "../models/busTrip.schema.js";
 import Bus from "../models/bus.schema.js";
+import BusTrip from "../models/busTrip.schema.js";
 import Location from "../models/location.schema.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  try {
-    const {
-      bus,
-      origin,
-      destination,
-      departureTime,
-      arrivalTime,
-      price,
-      status,
-    } = req.body;
+	try {
+		const {
+			bus,
+			assignedToUser,
+			status,
+			origin,
+			destination,
+			departureTime,
+			arrivalTime,
+			price,
+			amenity,
+			policy,
+			payment_methods,
+		} = req.body;
 
-    if (new Date(departureTime) >= new Date(arrivalTime)) {
-      return res.status(400).json({
-        message: "Departure time must be before arrival time",
-      });
-    }
+		if (new Date(departureTime) >= new Date(arrivalTime)) {
+			return res.status(400).json({
+				message: "Departure time must be before arrival time",
+			});
+		}
 
-    const existingBus = await Bus.findById(bus);
-    if (!existingBus) {
-      return res.status(404).json({ message: "Bus not found" });
-    }
+		const existingBus = await Bus.findById(bus);
+		if (!existingBus) {
+			return res.status(404).json({ message: "Bus not found" });
+		}
 
-    const existingOrigin = await Location.findById(origin);
-    if (!existingOrigin) {
-      return res.status(404).json({ message: "Origin location not found" });
-    }
+		const existingOrigin = await Location.findById(origin);
+		if (!existingOrigin) {
+			return res.status(404).json({ message: "Origin location not found" });
+		}
 
-    const existingDestination = await Location.findById(destination);
-    if (!existingDestination) {
-      return res.status(404).json({ message: "Destination not found" });
-    }
+		const existingDestination = await Location.findById(destination);
+		if (!existingDestination) {
+			return res.status(404).json({ message: "Destination not found" });
+		}
 
-    const newBusTrip = new BusTrip({
-      bus,
-      origin,
-      destination,
-      departureTime,
-      arrivalTime,
-      price,
-      status,
-    });
+		const newBusTrip = new BusTrip({
+			bus,
+			origin,
+			destination,
+			departureTime,
+			arrivalTime,
+			price,
+			status,
+			amenity,
+			policy,
+			payment_methods,
+		});
 
-    const savedBusTrip = await newBusTrip.save();
+		const savedBusTrip = await newBusTrip.save();
 
-    existingBus.trips.push(savedBusTrip._id);
-    await existingBus.save();
+		existingBus.trips.push(savedBusTrip._id);
+		await existingBus.save();
 
-    res.status(201).json(savedBusTrip);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error creating bus trip", error: error.message });
-  }
+		res.status(201).json(savedBusTrip);
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: "Error creating bus trip", error: error.message });
+	}
 });
 
 router.get("/", async (req, res) => {
-  try {
-    const {
-      fromCity,
-      toCity,
-      departureDate,
-      page = "1",
-      limit = "10",
-    } = req.query;
+	try {
+		const {
+			fromCity,
+			toCity,
+			departureDate,
+			page = "1",
+			limit = "10",
+		} = req.query;
 
-    if (!fromCity || !toCity || !departureDate) {
-      return res.status(400).json({
-        message: "fromCity, toCity, and departureDate are required",
-      });
-    }
+		if (!fromCity || !toCity || !departureDate) {
+			return res.status(400).json({
+				message: "fromCity, toCity, and departureDate are required",
+			});
+		}
 
-    const pageNumber = parseInt(page, 10);
-    const limitNumber = parseInt(limit, 10);
+		const pageNumber = parseInt(page, 10);
+		const limitNumber = parseInt(limit, 10);
 
-    const departureDateObj = new Date(departureDate);
-    const startOfDay = new Date(departureDateObj.setHours(0, 0, 0, 0));
-    const endOfDay = new Date(departureDateObj.setHours(23, 59, 59, 999));
+		const departureDateObj = new Date(departureDate);
+		const startOfDay = new Date(departureDateObj.setHours(0, 0, 0, 0));
+		const endOfDay = new Date(departureDateObj.setHours(23, 59, 59, 999));
 
-    const originLocations = await Location.find({ city: fromCity });
-    const destinationLocations = await Location.find({ city: toCity });
+		const originLocations = await Location.find({ city: fromCity });
+		const destinationLocations = await Location.find({ city: toCity });
 
-    if (!originLocations.length) {
-      return res
-        .status(404)
-        .json({ message: `Origin location with city ${fromCity} not found` });
-    }
+		if (!originLocations.length) {
+			return res
+				.status(404)
+				.json({ message: `Origin location with city ${fromCity} not found` });
+		}
 
-    if (!destinationLocations.length) {
-      return res.status(404).json({
-        message: `Destination location with city ${toCity} not found`,
-      });
-    }
+		if (!destinationLocations.length) {
+			return res.status(404).json({
+				message: `Destination location with city ${toCity} not found`,
+			});
+		}
 
-    let query = {
-      origin: { $in: originLocations.map((location) => location._id) },
-      destination: {
-        $in: destinationLocations.map((location) => location._id),
-      },
-      departureTime: { $gte: startOfDay, $lte: endOfDay }, // departureDate trùng với ngày yêu cầu
-    };
+		let query = {
+			origin: { $in: originLocations.map((location) => location._id) },
+			destination: {
+				$in: destinationLocations.map((location) => location._id),
+			},
+			departureTime: { $gte: startOfDay, $lte: endOfDay }, // departureDate trùng với ngày yêu cầu
+		};
 
-    const busTrips = await BusTrip.find(query)
-      .skip((pageNumber - 1) * limitNumber)
-      .limit(limitNumber)
-      .populate("origin", "name city")
-      .populate("destination", "name city");
+		const busTrips = await BusTrip.find(query)
+			.skip((pageNumber - 1) * limitNumber)
+			.limit(limitNumber)
+			.populate("origin", "name city")
+			.populate("destination", "name city");
 
-    const total = await BusTrip.countDocuments(query);
+		const total = await BusTrip.countDocuments(query);
 
-    const pagination = {
-      total,
-      pages: Math.ceil(total / limitNumber),
-      pageSize: limitNumber,
-      current: pageNumber,
-    };
+		const pagination = {
+			total,
+			pages: Math.ceil(total / limitNumber),
+			pageSize: limitNumber,
+			current: pageNumber,
+		};
 
-    res.status(200).json({
-      busTrips,
-      pagination,
-    });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ message: "Error fetching bus trips", error: error.message });
-  }
+		res.status(200).json({
+			busTrips,
+			pagination,
+		});
+	} catch (error) {
+		console.error(error);
+		res
+			.status(500)
+			.json({ message: "Error fetching bus trips", error: error.message });
+	}
 });
 
 router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updates = req.body;
+	try {
+		const { id } = req.params;
+		const updates = req.body;
 
-    if (!updates || Object.keys(updates).length === 0) {
-      return res.status(400).json({ message: "No fields to update" });
-    }
+		if (!updates || Object.keys(updates).length === 0) {
+			return res.status(400).json({ message: "No fields to update" });
+		}
 
-    const existingBusTrip = await BusTrip.findById(id);
-    if (!existingBusTrip) {
-      return res.status(404).json({ message: "BusTrip not found" });
-    }
+		const existingBusTrip = await BusTrip.findById(id);
+		if (!existingBusTrip) {
+			return res.status(404).json({ message: "BusTrip not found" });
+		}
 
-    if (updates.bus && updates.bus !== existingBusTrip.bus.toString()) {
-      const newBus = await Bus.findById(updates.bus);
-      if (!newBus) {
-        return res.status(404).json({ message: "New bus not found" });
-      }
+		if (updates.bus && updates.bus !== existingBusTrip.bus.toString()) {
+			const newBus = await Bus.findById(updates.bus);
+			if (!newBus) {
+				return res.status(404).json({ message: "New bus not found" });
+			}
 
-      const oldBus = await Bus.findById(existingBusTrip.bus);
-      if (oldBus) {
-        oldBus.trips = oldBus.trips.filter(
-          (tripId) => tripId.toString() !== id,
-        );
-        await oldBus.save();
-      }
+			const oldBus = await Bus.findById(existingBusTrip.bus);
+			if (oldBus) {
+				oldBus.trips = oldBus.trips.filter(
+					(tripId) => tripId.toString() !== id,
+				);
+				await oldBus.save();
+			}
 
-      newBus.trips.push(id);
-      await newBus.save();
-    }
+			newBus.trips.push(id);
+			await newBus.save();
+		}
 
-    if (
-      updates.departureTime &&
-      updates.arrivalTime &&
-      new Date(updates.departureTime) >= new Date(updates.arrivalTime)
-    ) {
-      return res
-        .status(400)
-        .json({ message: "Departure time must be before arrival time" });
-    }
+		if (
+			updates.departureTime &&
+			updates.arrivalTime &&
+			new Date(updates.departureTime) >= new Date(updates.arrivalTime)
+		) {
+			return res
+				.status(400)
+				.json({ message: "Departure time must be before arrival time" });
+		}
 
-    if (updates.origin) {
-      const originExists = await Location.findById(updates.origin);
-      if (!originExists) {
-        return res.status(404).json({ message: "Origin location not found" });
-      }
-    }
+		if (updates.origin) {
+			const originExists = await Location.findById(updates.origin);
+			if (!originExists) {
+				return res.status(404).json({ message: "Origin location not found" });
+			}
+		}
 
-    if (updates.destination) {
-      const destinationExists = await Location.findById(updates.destination);
-      if (!destinationExists) {
-        return res
-          .status(404)
-          .json({ message: "Destination location not found" });
-      }
-    }
+		if (updates.destination) {
+			const destinationExists = await Location.findById(updates.destination);
+			if (!destinationExists) {
+				return res
+					.status(404)
+					.json({ message: "Destination location not found" });
+			}
+		}
 
-    const updatedBusTrip = await BusTrip.findByIdAndUpdate(id, updates, {
-      new: true,
-    })
-      .populate("origin", "name city")
-      .populate("destination", "name city");
+		const updatedBusTrip = await BusTrip.findByIdAndUpdate(id, updates, {
+			new: true,
+		})
+			.populate("origin", "name city")
+			.populate("destination", "name city");
 
-    if (!updatedBusTrip) {
-      return res.status(404).json({ message: "BusTrip not found" });
-    }
+		if (!updatedBusTrip) {
+			return res.status(404).json({ message: "BusTrip not found" });
+		}
 
-    res.status(200).json(updatedBusTrip);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ message: "Error updating BusTrip", error: error.message });
-  }
+		res.status(200).json(updatedBusTrip);
+	} catch (error) {
+		console.error(error);
+		res
+			.status(500)
+			.json({ message: "Error updating BusTrip", error: error.message });
+	}
 });
 
 // TODO: GET trip by ID
